b991b9e99b31403f9401eb17a60118017e6ab334
Chris Griebel <chris.griebel.dev@gmail.com>
2023-03-09 13:23:35 -0500

    Added description to branch name btn

--- jira-branch-name-btn.js
+++ jira-branch-name-btn.js
@@ -2,7 +2,7 @@
 // @name         Get Branch Name
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Uses ticket type and description to generate a branch name
 // @author       You
 // @match        https://roger-team.atlassian.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=atlassian.net
